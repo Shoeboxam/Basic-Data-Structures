ecad40a6cc324143e317c4fb8b968ad87ff5d18b
Shoeboxam <shoeboxam@gmail.com>
2018-11-05 22:03:53 -0600

    file cleanup

--- Sandbox/HashTable.cpp
+++ Sandbox/HashTable.cpp
@@ -4,6 +4,17 @@
 #include <math.h>
 
 
+#include <iostream>
+
+
+HashTable::HashTable(std::string modex, int sizex, int alphabetx, float loadx) {
+	table = new std::string[sizex];
+	alphabet = alphabetx;
+	mode = modex;
+	size = sizex;
+	load = loadx;
+}
+
 int HashTable::isPrime(int val) {
 	for (int i = 2; i < sqrt(val); i++) if (val % i == 0) return false;
 	return true;
@@ -16,19 +27,63 @@ int HashTable::nextPrime(int val){
 	}
 }
 
-HashTable::HashTable(std::string modex, int sizex) {
-	table = new std::string[sizex]{ nullptr };
-	mode = modex;
-	size = sizex;
+void HashTable::rehash() {
+	int newSize = nextPrime(this->size * 2);
+	std::string* newTable = new std::string[newSize];
+
+	std::cout << "Resizing table from " << this->size << " to " << newSize << "." << std::endl;
+
+	for (int i = 0; i < this->size; i++) {
+		if (this->table[i] == "") continue;
+		this->insert(newTable, newSize, this->table[i]);
+	}
+	this->size = newSize;
+	this->table = newTable;
 }
 
-HashTable::~HashTable() {
+int HashTable::hash(std::string input) {
+	int hash = 0;
+	int multiplier = 1;
+
+	// compute hash
+	for (int i = 0; i < input.length(); i++) {
+		hash += ((int)tolower(input[i]) - (int) 'a') * multiplier;
+		multiplier *= this->alphabet;
+	}
+	return hash;
+}
+
+int HashTable::probe(std::string * table, int size, std::string input) {
+	// get table index
+	int idx = abs(this->hash(input)) % size;
+
+	int iteration = 0;
+	int offset = 0;
+
+	while (true) {
+		if (table[(idx + offset) % size] == "" || table[(idx + offset) % size] == input) return (idx + offset) % size;
+		this->collisions++;
+
+		iteration += 1;
+		if (this->mode == "linear") offset = iteration;
+		if (this->mode == "quadratic") offset = iteration * iteration;
+		//std::cout << table[(idx + offset) % size] << std::endl;
+	}
+}
+
+void HashTable::insert(std::string * table, int size, std::string input) {
+	table[this->probe(table, size, input)] = input;
 }
 
 void HashTable::insert(std::string input) {
-	int idx = stoi(input) % this->size;
+	// insert into table
+	this->insert(this->table, this->size, input);
+	this->length += 1;
 
-	//if (this->table[idx] == nullptr) {
-	//	this->table[idx] = input;
-	//}
+	// resize the table if necessary
+	if (this->length / (float) this->size > this->load) this->rehash();
+}
+
+bool HashTable::has(std::string input) {
+	return this->table[this->probe(this->table, this->size, input)] != "";
 }
--- Sandbox/HashTable.h
+++ Sandbox/HashTable.h
@@ -1,15 +1,27 @@
-
 #include <string>
 
 class HashTable {
-	std::string mode;
 	std::string* table;
+	std::string mode;
+
 	int size;
+	int length = 0;
+	int alphabet;
+	float load;
+
 	int isPrime(int val);
 	int nextPrime(int val);
-public:
-	HashTable(std::string mode, int size);
-	~HashTable();
-	void insert(std::string input);
-};
 
+	void rehash();
+	int hash(std::string input);
+
+	void insert(std::string* table, int size, std::string input);
+	int probe(std::string* table, int size, std::string input);
+
+public:
+	int collisions = 0;
+
+	HashTable(std::string modex, int sizex, int alphabetx, float loadx);
+	void insert(std::string input);
+	bool has(std::string input);
+};
